05ef0a280d72d71cef309e3688842190d83745f5
Reinaldo Henrique Morais <116378866+moraisReinaldo@users.noreply.github.com>
2026-09-17 21:23:21 -0300

    Automatizando xcodeBuild

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -3,11 +3,14 @@
 # Fail this script if any command fails.
 set -e
 
+# Disable CocoaPods stats to speed up and prevent network hang
+export COCOAPODS_DISABLE_STATS=true
+
 # The default execution directory of this script is the ci_scripts directory.
-cd $CI_PRIMARY_REPOSITORY_PATH
+cd "$CI_PRIMARY_REPOSITORY_PATH"
 
 # Install Flutter using git.
-git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable "$HOME/flutter"
 export PATH="$PATH:$HOME/flutter/bin"
 
 # Disable experimental Swift Package Manager in Flutter
@@ -19,11 +22,12 @@ flutter precache --ios
 # Install Flutter dependencies.
 flutter pub get
 
-# Generate iOS configuration and plugin registries
-flutter build ios --config-only --release
-
-# Install CocoaPods dependencies.
+# Install CocoaPods dependencies with automatic retries to handle CDN network timeouts
 cd ios
-pod install --repo-update
+pod install || (sleep 5 && pod install) || (sleep 10 && pod install)
+cd ..
+
+# Generate iOS configuration and plugin registries
+flutter build ios --config-only --release --no-codesign
 
 exit 0
